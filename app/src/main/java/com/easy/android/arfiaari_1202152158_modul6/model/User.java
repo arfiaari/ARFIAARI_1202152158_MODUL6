554f7120b2b46b2c2815582a377280b9997c818d
arfiaari <arfia.ari@gmail.com>
2018-04-01 22:15:27 +0700

    last goodbye

---
+++ app/src/main/java/com/easy/android/arfiaari_1202152158_modul6/model/User.java
@@ -0,0 +1,29 @@
+package com.easy.android.arfiaari_1202152158_modul6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
